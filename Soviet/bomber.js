3318f97a1f1b09a07aa5ad6997dca2c0c24b0bc6
Henrik Remmen <henrik.remmen@gmail.com>
2018-12-06 20:15:30 +0100

    commit 2

---
+++ Soviet/bomber.js
@@ -0,0 +1,29 @@
+const lvl1 = document.getElementById("level1");
+const lvl2 = document.getElementById("level2");
+const lvl3 = document.getElementById("level3");
+const level = document.getElementById("level");
+
+
+
+function f() {
+    a = 0;
+
+    if (a = 0) {
+        document.level.innerHTML = "Level: 0"
+    }   else if (a=1)   {
+        document.getElementById("level").innerHTML = "Level: 1";
+    }   else if (a=2)   {
+        document.level.innerHTML = "Level: 2"
+    }   else if (a=3)   {
+        document.level.innerHTML = "Level: 3"
+    }   else    {
+            document.level.innerHTML = "Broken system"
+        }
+    }
+}
+
+
+
+
+
+
